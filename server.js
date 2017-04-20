0a6e1521f2199a8cefb8759d7e161ee9c8aea4a5
onachassi <onachassi@gmail.com>
2017-04-19 21:50:29 -0700

    added projects page

--- server.js
+++ server.js
@@ -44,6 +44,17 @@ hbs.registerHelper('screamIt', (text) => {
 	return text.toUpperCase();
 })
 
+hbs.registerHelper('printProjects', (projects) => {
+	var html = ""
+	projects.forEach((project) => {
+		html += '<div class="project-container">'
+		html += '<h2 class="project-header">' + project.title + '</h2>'
+		html += '<p class"project-description">' + project.description + '</p>'
+		html += '</div>'
+	})
+	return html
+})
+
 // get request to root route takes two parameters: url and function
 // function has two variables request and response
 app.get('/', (req, res) => {
@@ -58,6 +69,22 @@ app.get('/', (req, res) => {
 app.get('/about', (req, res) => {
 	res.render('about.hbs', {
 		pageTitle: 'About Page'
+	})
+})
+
+app.get('/projects', (req, res) => {
+	res.render('projects.hbs', {
+		pageTitle: 'Project Page',
+		projects: [
+				{
+				title: "First Project",
+				description: "First node project!"
+			},
+			{
+				title: 'Second Project',
+				description: 'Second node project!'
+			}
+		]
 	})
 })
 // set up a new page /bad with error message obj
